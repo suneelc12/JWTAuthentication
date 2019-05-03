54e005a2b5187099ea9b97274d648f2f43aff1fe
suneel <suneelc12@gmail.com>
2019-05-03 18:35:01 +0530

    Adding files

---
+++ app/models/balanced.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// set up a mongoose model
+module.exports = mongoose.model('Balanced', new Schema({ 
+	name: String, 
+	attempts: Number, 
+	message: String
+}));
